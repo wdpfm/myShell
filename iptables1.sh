2902c1fb6041d7605b23349747d6dcc9838d8774
wdpfm <pfm9866@126.com>
2020-05-07 09:43:44 +0800

    iptables1

--- iptables1.sh
+++ iptables1.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
 #清空iptables规则
 iptables -F
+
 #设置默认策略
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
+
+#放行规则
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 80 -j ACCEPT
